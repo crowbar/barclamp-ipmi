e70c67e952501e83d28b6d8cccb98e2a3a8b86f6
jjacob512 <jacob_jacob@dell.com>
2013-07-15 15:31:32 -0500

    IPMI over lan privilges fix

--- chef/cookbooks/ipmi/providers/user_set.rb
+++ chef/cookbooks/ipmi/providers/user_set.rb
@@ -19,19 +19,17 @@ action :run do
   settle_time = new_resource.settle_time
 
   if ::File.exists?("/sys/module/ipmi_si")
-    if username == "root"
-      user_commands = [
-        [ "password", "ipmitool user set password 2 #{password}" ]
-      ]
-    else
-      user_commands = [
-        [ "name", "ipmitool user set name 3 #{username}" ],
-        [ "password", "ipmitool user set password 3 #{password}" ],
-        [ "privs", "ipmitool user priv 3 4 1" ],
-        [ "channel", "ipmitool channel setaccess 1 3 callin=on link=on ipmi=on privilege=4" ],
-        [ "enable", "ipmitool user enable 3" ]
-      ]
-    end
+    
+    # some assumptions on ID here!
+    username == "root" ? user_id = "2" : user_id = "3"
+
+    user_commands = [
+      [ "name", "ipmitool user set name #{user_id} #{username}" ],
+      [ "password", "ipmitool user set password #{user_id} #{password}" ],
+      [ "privs", "ipmitool user priv #{user_id} 4 1" ],
+      [ "channel", "ipmitool channel setaccess 1 #{user_id} callin=on link=on ipmi=on privilege=4" ],
+      [ "enable", "ipmitool user enable #{user_id}" ]
+    ]
 
     user_commands.each do |param|
       item = param[0]
